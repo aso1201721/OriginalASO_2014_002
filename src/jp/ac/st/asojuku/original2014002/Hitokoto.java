beeb79798e81ed35f68fdd365177c1eb859e8d65
student <student@PC111S16.fgakusei.local>
2014-06-11 11:17:26 +0900

    更新

--- src/jp/ac/st/asojuku/original2014002/Hitokoto.java
+++ src/jp/ac/st/asojuku/original2014002/Hitokoto.java
@@ -1,5 +1,15 @@
 package jp.ac.st.asojuku.original2014002;
 
-public class Hitokoto {
+import android.app.Activity;
+import android.os.Bundle;
 
+public class Hitokoto extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.hitokoto);
+
+		//TextView tv = (TextView)findViewById(R.id.txvMsg);
+		//tv.setText(inputMsg);
+	}
 }
+
--- src/jp/ac/st/asojuku/original2014002/MainActivity.java
+++ src/jp/ac/st/asojuku/original2014002/MainActivity.java
@@ -1,10 +1,13 @@
 package jp.ac.st.asojuku.original2014002;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +19,25 @@ public class MainActivity extends Activity {
 	protected void onResume() {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onResume();
-		bottn
+		Button btnmente = (Button)findViewById(R.id.btnmente);
+		btnmente.setOnClickListener(this);
+		Button btntoroku = (Button)findViewById(R.id.btntoroku);
+		btntoroku.setOnClickListener(this);
+		Button btncheck = (Button)findViewById(R.id.btncheck);
+		btncheck.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO 自動生成されたメソッド・スタブ
+		switch(v.getId()){
+			case R.id.btncheck:
+				EditText etv = (EditText)findViewById(R.id.edit);
+				String inputMsg = etv.getText().toString();
+
+
+		}
+
 	}
 
 	@Override
@@ -25,6 +46,6 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 
 }
